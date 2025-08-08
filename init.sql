d7b9ff72a14c3d4b23efbae214cfd7e35231814e
taizod1024 <taizod1024@gmail.com>
2025-08-09 08:37:12 +0900

    .envとdocker-compose.ymlを更新し、AppsmithとN8Nのデータベース設定を整理。init.sqlを削除。

--- init.sql
+++
@@ -1,7 +0,0 @@
--- Database initialization script
-\echo 'Creating anstack database...'
-
--- anstack database will be created automatically by POSTGRES_DB
-\echo 'Database anstack will be ready for Appsmith internal data and application data'
-
-\echo 'Database initialization completed!'

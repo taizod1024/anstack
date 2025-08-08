aadf66a05feac29bdc1c3a0d48613303a1b07cfa
taizod1024 <taizod1024@gmail.com>
2025-08-09 05:18:26 +0900

    PostgreSQLのデータベース名を変更し、init.sqlのコメントを整理

--- init.sql
+++ init.sql
@@ -1,12 +1,12 @@
 -- Appsmith and N8N database initialization script
 \echo 'Creating databases...'
 
--- Create N8N database
-CREATE DATABASE n8n OWNER postgres;
-\echo 'Created n8n database'
+-- -- Create N8N database
+-- CREATE DATABASE n8n OWNER postgres;
+-- \echo 'Created n8n database'
 
--- Create Appsmith database  
-CREATE DATABASE appsmith OWNER postgres;
-\echo 'Created appsmith database'
+-- -- Create Appsmith database  
+-- CREATE DATABASE appsmith OWNER postgres;
+-- \echo 'Created appsmith database'
 
 \echo 'Database initialization completed!'

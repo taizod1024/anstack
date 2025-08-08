67bce74895b4c7cbee9782d29057fadfc6f40528
taizod1024 <taizod1024@gmail.com>
2025-08-08 18:56:06 +0900

    .envとdocker-compose.ymlを更新し、Appsmithのデータベース設定を追加。init.sqlでデータベースの初期化スクリプトを修正。

--- init.sql
+++ init.sql
@@ -1,2 +1,12 @@
+-- Appsmith and N8N database initialization script
+\echo 'Creating databases...'
+
+-- Create N8N database
 CREATE DATABASE n8n OWNER postgres;
+\echo 'Created n8n database'
+
+-- Create Appsmith database  
 CREATE DATABASE appsmith OWNER postgres;
+\echo 'Created appsmith database'
+
+\echo 'Database initialization completed!'
